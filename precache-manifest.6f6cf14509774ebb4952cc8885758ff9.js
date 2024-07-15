897df38d14791089174b73de42ee48653830ffc0
ayesh-ayesha <113112082+ayesh-ayesha@users.noreply.github.com>
2024-07-15 09:45:04 +0500

    Updates

---
+++ precache-manifest.6f6cf14509774ebb4952cc8885758ff9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c97cdaf2524f70c2a17b7b37f4bb5d6",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "8e01a99bd1860c1c13ff",
+    "url": "/textutils/static/css/main.32b23ce9.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c2921800b298b2c621",
+    "url": "/textutils/static/js/3.65346564.chunk.js"
+  },
+  {
+    "revision": "8e01a99bd1860c1c13ff",
+    "url": "/textutils/static/js/main.f3d32ac5.chunk.js"
+  },
+  {
+    "revision": "948df55487e4db1d0fb0",
+    "url": "/textutils/static/js/runtime-main.b0db42f2.js"
+  }
+]);
